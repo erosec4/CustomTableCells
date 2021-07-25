4270d3d8d17f6b715fa90d44b385f54f0e262a2e
Emma Carpenetti <emmac4164@gmail.com>
2021-07-25 13:37:18 -0400

    Initial Commit

---
+++ CustomCellsTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomCellsTest
+//
+//  Created by Emma Carpenetti on 7/25/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
